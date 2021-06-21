e2c2f09e328d69aac7970e20e305b2e12cc17372
jagad <elarjeigidigiem@gmail.com>
2021-06-21 22:52:36 +0700

    /views - Disable unused field on Asesor read & write table

--- resources/views/create/asesor.blade.php
+++ resources/views/create/asesor.blade.php
@@ -3,8 +3,8 @@
 @section('content')
     <div class="container" style="background-color: white; border-radius: 10px;">
         <div class="row "> <!--justify-content-center-->
-            <div class="col-6">
-                <h1 class="mt-3">Tambah Data</h1>
+            <div class="col-12">
+                <h1 class="mt-3">Tambah Asesor</h1>
                 <hr>
                 <form method="POST" action="/index/asesor">
                     @csrf
@@ -14,23 +14,23 @@
                         </div>
                     @endif
                     <div class="form-group my-3">
-                        <label for="nama">Nama</label>
-                        <input type="text" class="form-control @error('nama') is-invalid @enderror" id="nama" name="nama" placeholder="Masukkan Nama" value="{{old('nama')}}">
+                        <label for="nama">Nama Lengkap</label>
+                        <input type="text" class="form-control @error('nama') is-invalid @enderror" id="nama" name="nama" placeholder="masukkan nama lengkap" value="{{old('nama')}}">
                         @error('nama') <div class="invalid-feedback"> {{$message}} </div> @enderror
                     </div>
-                    <div class="form-group my-3">
+                    {{-- <div class="form-group my-3">
                         <label for="nim">NIM</label>
-                        <input type="text" class="form-control  @error('nim') is-invalid @enderror" id="nim" name="nim" placeholder="Masukkan nim" value="{{old('nim')}}">
+                        <input type="text" class="form-control  @error('nim') is-invalid @enderror" id="nim" name="nim" placeholder="masukkan NIM" value="{{old('nim')}}">
                         @error('nim') <div class="invalid-feedback"> {{$message}} </div> @enderror
                     </div>
                     <div class="form-group my-3">
                         <label for="nik">NIK</label>
-                        <input type="text" class="form-control  @error('nik') is-invalid @enderror" id="nik" name="nik" placeholder="Masukkan nik" value="{{old('nik')}}">
+                        <input type="text" class="form-control  @error('nik') is-invalid @enderror" id="nik" name="nik" placeholder="masukkan NIK" value="{{old('nik')}}">
                         @error('nik') <div class="invalid-feedback"> {{$message}} </div> @enderror
                     </div>
                     <div class="form-group my-3">
                         <label for="tempat_lahir">Tempat Lahir</label>
-                        <input type="text" class="form-control  @error('tempat_lahir') is-invalid @enderror" id="tempat_lahir" name="tempat_lahir" placeholder="Tempat Lahir" value="{{old('tempat_lahir')}}">
+                        <input type="text" class="form-control  @error('tempat_lahir') is-invalid @enderror" id="tempat_lahir" name="tempat_lahir" placeholder="tempat lahir" value="{{old('tempat_lahir')}}">
                         @error('tempat_lahir') <div class="invalid-feedback"> {{$message}} </div> @enderror
                     </div>
                     <div class="form-group my-3">
@@ -49,15 +49,15 @@
                         @error('alamat') <div class="invalid-feedback"> {{$message}} </div> @enderror
                     </div>
                     <div class="form-group my-3">
-                        <label for="no_telpon">No Telepon</label>
-                        <input type="tel" class="form-control  @error('no_telpon') is-invalid @enderror" id="no_telpon" name="no_telpon" placeholder="masukkan nomor telepon" value="{{old('no_telpon')}}">
+                        <label for="no_telpon">No HP/Telepon</label>
+                        <input type="tel" class="form-control  @error('no_telpon') is-invalid @enderror" id="no_telpon" name="no_telpon" placeholder="masukkan no telepon" value="{{old('no_telpon')}}">
                         @error('no_telpon') <div class="invalid-feedback"> {{$message}} </div> @enderror
                     </div>
                     <div class="form-group my-3">
                         <label for="email">Email</label>
                         <input type="email" class="form-control  @error('email') is-invalid @enderror" id="email" name="email" placeholder="masukkan email" value="{{old('email')}}">
                         @error('email') <div class="invalid-feedback"> {{$message}} </div> @enderror
-                    </div>
+                    </div> --}}
                     <button type="submit" class="btn bg-primary" style="color: white">Tambah Data</button>
                     <a href="{{ url('/index/dataasesor') }}" class="btn bg-warning my-3" style="color: white">Lihat Data</a>
                     <a href="{{ url('/home') }}" class="btn bg-success my-3" style="color: white">Kembali ke Homepage</a>
